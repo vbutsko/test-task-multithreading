c3f025e468fca26c96873801df6ebb19a350a5fe
wladislawbucko <studing1995@gmail.com>
2017-01-25 12:41:18 +0300

    changed logic in

--- src/org/expertsoft/executor/impl/ExecutorSynchronousQueueImpl.java
+++ src/org/expertsoft/executor/impl/ExecutorSynchronousQueueImpl.java
@@ -1,7 +1,40 @@
 package org.expertsoft.executor.impl;
 
+import org.expertsoft.executor.Executor;
+import org.expertsoft.executor.TaskAccess;
+import org.expertsoft.executor.thread.ExecuteThread;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.SynchronousQueue;
+
 /**
  * Created by wladek on 1/24/17.
  */
-public class ExecutorSynchronousQueueImpl {
+public class ExecutorSynchronousQueueImpl implements Executor, TaskAccess {
+    private final BlockingQueue<Runnable> queueOfTasks;
+    private final ExecuteThread executeThread;
+
+    public ExecutorSynchronousQueueImpl(){
+        queueOfTasks = new SynchronousQueue<Runnable>();
+        executeThread = new ExecuteThread(this);
+        executeThread.start();
+    }
+
+    public Runnable getNextTask(){
+        try {
+            return queueOfTasks.take();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            return null;
+        }
+    }
+
+    public void execute(Runnable task){
+        try {
+            queueOfTasks.put(task);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        executeThread.myNotify();
+    }
 }
